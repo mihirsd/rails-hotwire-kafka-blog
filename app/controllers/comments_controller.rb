623a7177560ac83a7c005ae0ab63440f725bea7f
Mihir Desai <mihir@mihirdesai.net>
2025-10-15 17:55:26 +0530

    Setup routes and controllers for posts and comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+
+    if @comment.save
+      redirect_to posts_path, notice: "Comment added successfully!"
+    else
+      redirect_to posts_path, alert: "Failed to create comment"
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :author_name)
+  end
+end
